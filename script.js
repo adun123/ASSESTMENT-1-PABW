420a64d50162abfb561febc98889666ecb8ee8e6
abdurrahman <abdurrahmanaikon@gmail.com>
2024-04-05 14:01:26 +0700

    v.3 membuat kondisi saat jumlah dan pilihan buku tidak di isi

--- script.js
+++ script.js
@@ -11,5 +11,11 @@ function cekbuku() {
     let jumlah = document.getElementById("jumlah").value;
     let pilihan = document.getElementById("pilihan").value;
 
+    if (jumlah== "" || pilihan == "") {
+        alert("Harap isi jumlah dan pilihan buku");
+        return;
+        
+    }
+
     
 }
